c3706a2363dfbc1003e9edbf636c3a06f6174f3c
Adrian Mostowski <adaxelx@o2.pl>
2019-06-06 14:11:08 +0200

    reset + add global styles

---
+++ src/GlobalStyle/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=VT323&display=swap');
+  *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-family: 'VT323', monospace;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    font-size: 1.6rem;
+  }
+`;
+
+export default GlobalStyle;
