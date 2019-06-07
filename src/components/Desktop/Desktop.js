c94ea6f2c6c3bb63ebbbd05a4237556b7d6d2d0b
Adrian Mostowski <adaxelx@o2.pl>
2019-06-07 23:53:14 +0200

    add next icon + map Icons

--- src/components/Desktop/Desktop.js
+++ src/components/Desktop/Desktop.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 import Wallpaper from '../../img/wallpaper.jpg';
 import Icon from './Icon';
 
+/* imgs */
+
+import pc from '../../img/komputer.png';
+import bin from '../../img/kosz.png';
+
 const StyledDesktop = styled.main`
     height: 100vh;
     width: 100%
@@ -13,12 +18,21 @@ const StyledDesktop = styled.main`
     padding: 5px
 `;
 
+const images = [
+  {
+    src: pc,
+    content: 'Komputer',
+  },
+  {
+    src: bin,
+    content: 'Kosz',
+  },
+];
+
 const Desktop = () => {
-  return (
-    <StyledDesktop>
-      <Icon />
-    </StyledDesktop>
-  );
+  const Icons = images.map(Icone => <Icon src={Icone.src} content={Icone.content} />);
+
+  return <StyledDesktop>{Icons}</StyledDesktop>;
 };
 
 export default Desktop;
--- src/components/Desktop/Icon.js
+++ src/components/Desktop/Icon.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
-import pc from '../../img/komputer.png';
 
-const StyledIcon = styled.section`
+const StyledIcon = styled.button`
     height: 60px
     width: 72px
     display: flex
     flex-direction: column
     align-items: center
+    background-color: transparent;
+    border: none;
 `;
 
 const StyledImg = styled.img`
   width: 32px;
   height: 32px;
+  background-blend-mode: blue;
 `;
 
 const StyledTitle = styled.p`
     height: 28px;
     line-height: 28px
     color: white
+
 `;
 
-const Icon = () => {
+const Icon = ({ src, content }) => {
   return (
     <StyledIcon>
-      <StyledImg src={pc} />
-      <StyledTitle>Komputer</StyledTitle>
+      <StyledImg src={src} />
+      <StyledTitle>{content}</StyledTitle>
     </StyledIcon>
   );
+};
+
+Icon.propTypes = {
+  src: String.isRequired,
+  content: String.isRequired,
 };
 
 export default Icon;
